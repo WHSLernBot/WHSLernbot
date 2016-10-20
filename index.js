384bb0807088d00dce092e4deff4bef239d7259a
WHSLernBot <whslernbot@gmail.com>
2016-10-20 13:35:54 +0200

    Hinzufügen der firstEntityValue

--- index.js
+++ index.js
@@ -103,6 +103,20 @@ const findOrCreateSession = (fbid) => {
   return sessionId;
 };
 
+
+const firstEntityValue = (entities, entity) => {
+    const val = entities && entities[entity] &&
+            Array.isArray(entities[entity]) &&
+            entities[entity].length > 0 &&
+            entities[entity][0].value
+        ;
+    if (!val) {
+        return null;
+    }
+    return typeof val === 'object' ? val.value : val;
+};
+
+
 // Innerhalb von Actions müssen unsere Funktionen reingepackt werden
 const actions = {
 	
