128ca75934744527be6d2ab5ec7749a91b4a7a45
WHSLernBot <whslernbot@gmail.com>
2016-10-27 23:29:15 +0200

    Test

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const crypto = require('crypto');
 const express = require('express');
 const fetch = require('node-fetch');
 const request = require('request');
-
+const http = require('http');
 let Wit = null;
 let log = null;
 
@@ -157,6 +157,14 @@ const actions = {
   //Eine Test Funktion
   getForecast({context, entities}) {
   return new Promise(function(resolve, reject) {
+	  
+	http.get('https://secure-plateau-70134.herokuapp.com/test', function(response) {
+		
+		console.dir(response);
+		
+	});
+	  
+	  
     var location = firstEntityValue(entities, "location")
     if (location) {
       context.forecast = 'sonnig in ' + location; // we should call a weather API here
