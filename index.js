07773ed1ee613fc5a1a54e1d2ecf666c264db237
WHSLernBot <whslernbot@gmail.com>
2016-11-11 11:04:11 +0100

    gibAufgabeFunktion

--- index.js
+++ index.js
@@ -135,25 +135,28 @@ const actions = {
     
     if (recipientId) {
 		
-        text = "attachment":{
-        "type":"template",
-        "payload":{
-        "template_type":"button",
-        "text":"What do you want to do next?",
-        "buttons":[
-         {
-            "type":"web_url",
-            "url":"www.google.de",
-            "title":"Show Website"
-         },
-         {
-            "type":"postback",
-            "title":"Start Chatting",
-            "payload":"USER_DEFINED_PAYLOAD"
-         }
-        ]
-        }
-        };
+        text =  {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [{
+                            "title": "Kitten",
+                            "subtitle": "Cute kitten picture",
+                            "image_url": imageUrl ,
+                            "buttons": [{
+                                "type": "web_url",
+                                "url": imageUrl,
+                                "title": "Show kitten"
+                                }, {
+                                "type": "postback",
+                                "title": "I like this",
+                                "payload": "User " + recipientId + " likes kitten " + imageUrl,
+                            }]
+                        }]
+                    }
+                }
+            };
         
       // Yay, we found our recipient!
       // Let's forward our bot response to her.
