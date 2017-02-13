caa1412c8a4303703e020c4d2974dab5cb302ee6
WHSLernBot <whslernbot@gmail.com>
2017-02-13 16:27:40 +0100

    gibAufgabe neu

--- index.js
+++ index.js
@@ -357,33 +357,42 @@ const actions = {
     
     
 
-    loese( {context, entities, sessionId}) {
+    replies( {context, entities, sessionId}) {
         return new Promise(function (resolve, reject) {
+            
+            var sender = sessions[sessionId].fbid;
 
+            var frage = {
+                        "text":"Welche Antwort meinst du ist richtig?",
+                        "quick_replies":[
+                        {
+                          "content_type":"text",
+                          "title":"A",
+                          "payload":"richtig"
+                        },
+                        {
+                          "content_type":"text",
+                          "title":"B",
+                          "payload":"falsch"
+                        },
+                        {
+                          "content_type":"text",
+                          "title":"C",
+                          "payload":"falsch"
+                        }
+                      ]
+              };
 
-            delete context.modul;
-            delete context.thema;
-            delete context.missingThema;
-            delete context.missingModul;
-
-            var antwort = firstEntityValue(entities, "antwort");
-
-            if (antwort) {
-                switch (antwort) {
-
-                    case '(B)':
-                        context.antwort = "Falsch";
-                        break;
-                    case '(C)':
-                        context.antwort = "Leider falsch :S";
-                        break;
-                    default:
-                        context.antwort = "Richtig :)";
-
-
-                }
-
-            }
+            fbMessage(sender, frage)
+                    .then(() => null)
+                    .catch((err) => {
+                        console.error(
+                                'Oops! An error occurred while forwarding the response to',
+                                sender,
+                                ':',
+                                err.stack || err
+                                );
+                    });
 
 
             return resolve(context);
@@ -451,95 +460,6 @@ const actions = {
                 context.A = "Aussage A";
                 context.B = "Aussage B";
                 context.C = "Aussage C";
-                
-//                var aufgabe = {"text": "Das ist die Aufgabe über " + modul};
-//                var a = {"text": "Das ist Aussage A"};
-//                var b = {"text": "Das ist Aussage B"};
-//                var c = {"text": "Das ist Aussage C"};
-//                var frage = {
-//                              "message" :{
-//                              "text":"Welche Antwort meinst du ist richtig?",
-//                              "quick_replies":[
-//                                {
-//                                  "content_type":"text",
-//                                  "title":"A",
-//                                  "payload":"richtig"
-//                                },
-//                                {
-//                                  "content_type":"text",
-//                                  "title":"B",
-//                                  "payload":"falsch"
-//                                },
-//                                {
-//                                  "content_type":"text",
-//                                  "title":"C",
-//                                  "payload":"falsch"
-//                                }
-//                              ]
-//                          }
-//                      };
-                            
-                          
-                          
-//                var sender = sessions[sessionId].fbid;
-                
-//                fbMessage(sender, aufgabe)
-//                    .then(() => null)
-//                    .catch((err) => {
-//                        console.error(
-//                                'Oops! An error occurred while forwarding the response to',
-//                                sender,
-//                                ':',
-//                                err.stack || err
-//                                );
-//                    });
-//                    
-//                
-//                
-//                fbMessage(sender, a)
-//                    .then(() => null)
-//                    .catch((err) => {
-//                        console.error(
-//                                'Oops! An error occurred while forwarding the response to',
-//                                sender,
-//                                ':',
-//                                err.stack || err
-//                                );
-//                    });
-//                    
-//                fbMessage(sender, b)
-//                    .then(() => null)
-//                    .catch((err) => {
-//                        console.error(
-//                                'Oops! An error occurred while forwarding the response to',
-//                                sender,
-//                                ':',
-//                                err.stack || err
-//                                );
-//                    });
-//                    
-//                fbMessage(sender, c)
-//                    .then(() => null)
-//                    .catch((err) => {
-//                        console.error(
-//                                'Oops! An error occurred while forwarding the response to',
-//                                sender,
-//                                ':',
-//                                err.stack || err
-//                                );
-//                    });
-                    
-                
-//                fbMessage(sender, frage)
-//                    .then(() => null)
-//                    .catch((err) => {
-//                        console.error(
-//                                'Oops! An error occurred while forwarding the response to',
-//                                sender,
-//                                ':',
-//                                err.stack || err
-//                                );
-//                    });
                 
                 context.modul = modul;
                 delete context.missingModul;
