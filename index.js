4a5a71a38f9638dd8ba3f07f61e39dc33cdbcff5
WHSLernBot <whslernbot@gmail.com>
2017-02-22 14:52:14 +0100

    meldeModulAN

--- index.js
+++ index.js
@@ -917,6 +917,7 @@ const actions = {
             var modul;
             var i = 0;
             console.log("Vor der While schleife");
+            console.log(entities);
             console.log(entities.entity);
             console.log(entities[entity].length);
             while(entities[entity].length > i) {
